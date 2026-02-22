c30a0f880459c46d0ae89fdf2916783ccb511dc0
BajiLinux <mr.baji.smart@gmail.com>
2026-02-23 05:35:28 +0700

    Tinggal beresin mappingnya aja hehew udah pagi mau bobokduulu

--- 2-Landing_Page/app.js
+++ 2-Landing_Page/app.js
@@ -1,3 +1,25 @@
 // ? ./app.js
-import getAPI from "./getAPI";
+import getAPI from "./getAPI.js";
 const content = document.getElementById("content");
+
+const productRender = async () => {
+  try {
+    // Suruh sabar dulu usernya
+    content.innerHTML = `<p>Sabar lagi loading...</p>`
+    // Tahap consume API
+    const product = await getAPI();
+    
+    return `${product.map(product => {
+      content.innerHTML = `<ul>
+        <li>
+          ${product}
+        </li>
+      </ul>`
+    })}`
+    
+  } catch (error) {
+    throw new Error(`Yah error ${error}`);
+  };
+};
+
+productRender();
