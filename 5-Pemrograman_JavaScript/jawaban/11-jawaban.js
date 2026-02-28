3a642d509dcd8f404a41734a6d6205dddb775246
BajiLinux <mr.baji.smart@gmail.com>
2026-02-28 20:41:18 +0700

    11 Done lanjut 12

---
+++ 5-Pemrograman_JavaScript/jawaban/11-jawaban.js
@@ -0,0 +1,10 @@
+const profilUser = {
+  username: "BajiKeisuke",
+  level: 99,
+  role: "Kapten Divisi 1",
+  status: "Aktif"
+};
+
+const {username, level, role, status} = profilUser;
+
+console.log(`Halo ${role} ${username}! Level Anda: ${level} Status: ${status}`)
