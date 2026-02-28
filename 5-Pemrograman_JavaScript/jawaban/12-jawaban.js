9a6901d18752480923383ab7742afd202a0778ea
BajiLinux <mr.baji.smart@gmail.com>
2026-02-28 21:04:04 +0700

    12 done -> 13

---
+++ 5-Pemrograman_JavaScript/jawaban/12-jawaban.js
@@ -0,0 +1,11 @@
+
+const statusBaji = {
+  username: "BajiKeisuke",
+  level: 99,
+  darah: 1000,
+  divisi: 1
+};
+
+const bajiModeMarah = {...statusBaji, senjata: "Pipa Besi", darah: 5000};
+
+console.log(bajiModeMarah);
