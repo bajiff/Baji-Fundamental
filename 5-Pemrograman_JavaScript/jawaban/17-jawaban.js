14899d4f35855d02bb080163caa29e1a7429e0ef
BajiLinux <mr.baji.smart@gmail.com>
2026-03-01 17:09:14 +0700

    17-Done->18

--- 5-Pemrograman_JavaScript/jawaban/17-jawaban.js
+++ 5-Pemrograman_JavaScript/jawaban/17-jawaban.js
@@ -1,15 +1,17 @@
-const getAPI = async () => {
-  const api = "https://jsonplaceholder.typicode.com/posts";
-  try {
-    const response = await fetch(api);
-    const json = await response.json();
-    console.log(json);
-    return json;
-  } catch (error) {
-    throw new Error(`Yahh ada error: ${error}`);
-  } finally {
-    console.log("Berhasil Ngambil API");
-  };
-};
+let saldo = 50000;
+let tarikTunai = 200000;
 
-getAPI();
+console.log("Memulai transaksi...");
+
+try {
+  if (tarikTunai > saldo){
+    throw new Error("Sadar diri woy saldo lu ngga cukup, dasar miskin");
+  } else {
+    saldo -= tarikTunai;
+    console.log("Penarikan berhasil! Sisa saldo: " + saldo)
+  };
+} catch (error) {
+  console.log(`Mohon maaf, transaksi dibatalkan. Sistem mendeteksi anomali: ${error.message}`);
+} finally {
+  console.log("Terimakasih telah menggunakan ATM kami. Jangan lupa gunakan uang sebaik mungkin");
+}
