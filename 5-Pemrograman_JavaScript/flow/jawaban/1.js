4a8f8af737e886c03c10c1f9ff136a9ac89ef7d1
BajiLinux <mr.baji.smart@gmail.com>
2026-03-02 15:03:50 +0700

    Beresin flow udah done

--- 5-Pemrograman_JavaScript/flow/jawaban/1.js
+++ 5-Pemrograman_JavaScript/flow/jawaban/1.js
@@ -1,11 +1,15 @@
 function categorizeNumber(input) {
   if (typeof(input) !== "number") {
     throw new Error("Input harus berupa bilangan bulat");
-  } else if (input === 0) {
+  }
+  if (input === 0) {
     return "Nol";
-  } else if (input < 0) {
+  }
+  if (input < 0) {
     return "Negatif";
-  } else if (input > 1){
+  }
+
+  if (input > 1){
     // ? Inisialisasi awal dibikin true
     let apakahPrima = true;
     
@@ -18,12 +22,12 @@ function categorizeNumber(input) {
         break;
       } 
     }
-      if (apakahPrima) {
-        return "Prima"
-      }
-  } else if (input % 2 === 0) {
+    if (apakahPrima) {
+      return "Prima";
+    };
+  } if  (input % 2 === 0) {
     return "Genap";
-  } else if (input % 2 === 1) {
+  } if (input % 2 === 1) {
     return "Ganjil";
   }
 }
@@ -34,7 +38,6 @@ console.log(categorizeNumber(12)); // Output: "Genap"
 console.log(categorizeNumber(17)); // Output: "Prima"
 console.log(categorizeNumber(0)); // Output: "Nol"
 console.log(categorizeNumber(-5)); // Output: "Negatif"
-console.log(17 % 3)
 try {
   categorizeNumber('abc');
 } catch (error) {
