6ada3ad3ce30fedb28838e2ceb4ca3386cfac82f
BajiLinux <mr.baji.smart@gmail.com>
2026-03-04 18:49:38 +0700

    Ronde 1

---
+++ 5-Pemrograman_JavaScript/asynchronous/jawaban/1-jawaban.js
@@ -0,0 +1,24 @@
+const pictures = ["1.jpg","Epstein.jpg","Fatamorgana.jpg"];
+const token = ~~[Math.random() * 123456789];
+
+const login = (username) => {
+  return {token, username};
+};
+
+const getAPI_KEY = (token) => {
+  if (token) {
+    return {apiKey:"baji-api-key"};
+  };
+};
+
+const getPictures = (apiKey) => {
+  if (apiKey) {
+    return pictures;
+  }
+}
+
+const user = login("Baji");
+const apiKey = getAPI_KEY(user.token);
+const picture = getPictures(apiKey)
+
+console.log(user,picture);
