a98505486bcf679c75a8078f9757b948cde48929
BajiLinux <mr.baji.smart@gmail.com>
2026-03-05 22:15:50 +0700

    Ada salah logika sedikit, seharusnya saya menggunakan setTimeout bukan setInterval soalnya kalau setInterval itu menjalankan kode setelah beberapa detik dan terus dijalankan berbeda dengan setTimeout atau set waktu selesai, jadi menunggu beberapa detik dulu wakktunya lalu kodenya dijalankan SEKALI SAJA bukan berkalikali seperti setInterval

--- 5-Pemrograman_JavaScript/asynchronous/jawaban/coba.js
+++ 5-Pemrograman_JavaScript/asynchronous/jawaban/coba.js
@@ -1,14 +1,40 @@
-const beliMieAyam = new Promise((resolve,reject) => {
-  let duit = 50000;
-  let hargaMieAyam = 10000;
+// const janjiBeliMotor = new Promise((resolve,reject) => {
+//   let duitnya = 3000;
+//   let hargaMotor = 300000;
+//   setTimeout(() => {
+//     if (duitnya >= hargaMotor) {
+//       resolve(`Transaksi Berhasil saldo Anda ${duitnya}, Harga Motor: ${hargaMotor} Sisa saldo: ${duitnya - hargaMotor}`)
+//     } else {
+//       reject(new Error("Kirim duite kurang cok, saldo anda " + duitnya));
+//     };
+//   },3000);
+// });
 
-  setInterval(() => {
-    if (duit >= hargaMieAyam) {
-      resolve("Duit nya ngga kurang");
-    } else {
-      reject("Duit nya kurang oy");
-    };
-  },3000);
-});
+// console.log("Beli motor");
+// janjiBeliMotor.then(onfulfilled => {
+//   console.log(`Yey ${onfulfilled}`);
+// }).catch(onrejected => {
+//   console.log(`Yahhhh ${onrejected}`)
+// })
+// console.log("Nunggu beli motor");
+// 
+const beliKuota = (duit = 0) => {
+  return new Promise((resolve,reject) => {
+    console.log("Proses pembelian kuota");
+    let hargaKuota = 5000;
+    setTimeout(() => {
+      if (duit >= hargaKuota){
+        console.log("Harga Kuota: " + hargaKuota);
+        resolve(`Yey berhasil, sisa uang: ${duit - hargaKuota}`);
+      } else {
+        reject(new Error(`Duit lu kureng dek, sana ambil duit lagi ${hargaKuota - duit}`));
+      };
+    },2000)
+  });
+};
 
-console.log(beliMieAyam());
+beliKuota(2000).then(onfulfilled => console.log("Berhasil " + onfulfilled))
+.catch(onrejected => console.log("Yahh gagal " + onrejected.message))
+
+beliKuota(5000).then(onfulfilled => console.log("Berhasil " + onfulfilled))
+.catch(onrejected => console.log("Yahh gagal " + onrejected.message))
