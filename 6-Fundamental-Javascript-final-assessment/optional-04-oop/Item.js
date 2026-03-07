864093c6848d269e6795cb00d357f0146a4eea8c
BajiLinux <mr.baji.smart@gmail.com>
2026-03-07 13:03:03 +0700

    feat

--- 6-Fundamental-Javascript-final-assessment/optional-04-oop/Item.js
+++ 6-Fundamental-Javascript-final-assessment/optional-04-oop/Item.js
@@ -24,7 +24,7 @@ class Item {
   };
   
   displayDetails(){
-    return `ID: ${this.id}, Quantity: ${this.quantity}, Price: ${this.price}`
+    return `ID: ${this.id}, Name: ${this.name} Quantity: ${this.quantity}, Price: ${this.price}`
   }
 
 };
