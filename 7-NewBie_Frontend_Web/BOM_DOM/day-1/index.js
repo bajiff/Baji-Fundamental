a74f801481265787a063ffaf8a411f73750578ba
BajiLinux <mr.baji.smart@gmail.com>
2026-03-07 21:47:56 +0700

    day-1 cobain DOM

--- 7-NewBie_Frontend_Web/BOM_DOM/day-1/index.js
+++ 7-NewBie_Frontend_Web/BOM_DOM/day-1/index.js
@@ -1,6 +1,11 @@
 const formTambahLogistik = document.getElementById("form-tambah-logistik");
+const inputBarang = document.getElementById("barang");
+const inputJumlah = document.getElementById("jumlah");
+const result = document.getElementById("result");
 formTambahLogistik.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  console.log("Gebiel Kirim");
+  const outputInputBarang = inputBarang.value;
+  const outputInputJumlah = inputJumlah.value;
+  result.innerText = (`Menambahkan : ${outputInputJumlah} unit ${outputInputBarang} ke dalam gudang`);
+  console.log(`Menambahkan : ${outputInputJumlah} unit ${outputInputBarang} ke dalam gudang`)
 })
