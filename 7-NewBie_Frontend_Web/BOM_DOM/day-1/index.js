e2ebc075ef14975c8b6aa9e6460deaf42d855e60
BajiLinux <mr.baji.smart@gmail.com>
2026-03-07 14:42:08 +0700

    Percobaan Pertama

---
+++ 7-NewBie_Frontend_Web/BOM_DOM/day-1/index.js
@@ -0,0 +1,6 @@
+const formTambahLogistik = document.getElementById("form-tambah-logistik");
+formTambahLogistik.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  console.log("Gebiel Kirim");
+})
